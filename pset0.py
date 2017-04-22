5b5f5aa40c7a90569f124a3e5c01a16f67ded573
OlenaNechai <nechaiolena@gmail.com>
2017-04-22 22:25:53 +0300

    Add files via upload

---
+++ pset0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 14:32:35 2017
+
+@author: Ярик
+"""
+import math
+a = int(input("Please enter a number x: "))
+b = int(input("Please enter a number y: "))
+print("x**y =",a**b)
+print("log(x) =",int(math.log2(a)))
